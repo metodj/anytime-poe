0cb69bf5c05de3e2fece17bada2111d82dbef38f
metodj <jazbec.metod@gmail.com>
2022-12-05 18:53:26 +0100

    ColorMNIST results

---
+++ experiments/configs/color_mnist_hard_ovr_classification.py
@@ -0,0 +1,61 @@
+from math import ceil
+
+from ml_collections import config_dict
+
+from src.data import METADATA, train_val_split_sizes
+
+
+def get_config() -> config_dict.ConfigDict:
+    config = config_dict.ConfigDict()
+
+    config.dataset_name = "ColorMNIST"
+    config.val_percent = 0.1
+    config.batch_size = 512
+    config.epochs = 25
+
+    config.optim_name = "sgdw"
+    config.optim = config_dict.ConfigDict()
+    config.optim.weight_decay = 1e-4
+    config.optim.momentum = 0.9
+    config.learning_rate = 3e-3
+
+    num_train, _ = train_val_split_sizes(
+        METADATA["num_train"][config.dataset_name], config.val_percent
+    )
+    num_batches_per_epoch = ceil(num_train / config.batch_size)
+
+    config.lr_schedule_name = "warmup_cosine_decay_schedule"
+    config.lr_schedule = config_dict.ConfigDict()
+    config.lr_schedule.peak_value = 3 * config.learning_rate
+    config.lr_schedule.end_value = 1 / 10 * config.learning_rate
+    config.lr_schedule.decay_steps = config.epochs * num_batches_per_epoch
+    config.lr_schedule.warmup_steps = int(0.2 * config.lr_schedule.decay_steps)
+
+    config.model_name = "Hard_OvR_Ens"
+    config.model = config_dict.ConfigDict()
+    config.model.size = 5
+    config.model.learn_weights = False
+    config.model.net_type = "ConvNetRGB"
+
+    config.β_schedule = config_dict.ConfigDict()
+    config.β_schedule.name = "sigmoid"
+    config.β_schedule.start = 2.0
+    config.β_schedule.end = 16.0
+    config.β_schedule.steps = config.epochs * num_batches_per_epoch
+
+    # config.β = 1.
+
+    config.alpha_schedule = config_dict.ConfigDict()
+    config.alpha_schedule.name = "sigmoid"
+    config.alpha_schedule.start = 0.0
+    config.alpha_schedule.end = 1.0
+    config.alpha_schedule.steps = config.epochs * num_batches_per_epoch
+    # config.alpha = 1.
+
+    config.model.net = config_dict.ConfigDict()
+    config.model.net.depth = 5
+    config.model.net.hidden_size = 200
+    config.model.net.out_size = 10
+    config.model.net.p_drop = .1
+
+    return config
--- src/data/color_mnist.py
+++ src/data/color_mnist.py
@@ -3,8 +3,17 @@ import os
 import pandas as pd
 import numpy as np
 
+
 class ColorMNIST(Dataset):
-    def __init__(self, img_dir="/home/metod/datasets/color_mnist/", transform=None, target_transform=None, train=True, download=False, root=False):
+    def __init__(
+        self,
+        img_dir="/home/metod/datasets/color_mnist/",
+        transform=None,
+        target_transform=None,
+        train=True,
+        download=False,
+        root=False,
+    ):
         """
         root and download args are there to ensure compatibility with get_image_dataset function in image.py
         """
@@ -17,7 +26,6 @@ class ColorMNIST(Dataset):
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
-        
 
     def __len__(self):
         return len(self.img_labels)
@@ -27,7 +35,7 @@ class ColorMNIST(Dataset):
         image = np.load(img_path)
         digit_label = self.img_labels.iloc[idx, 1]
         color_label = self.img_labels.iloc[idx, 2]
-        color_dict = self.img_labels.iloc[idx, 3]
+        color_label_random = self.img_labels.iloc[idx, 3]
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
@@ -35,4 +43,42 @@ class ColorMNIST(Dataset):
         if self.train:
             return image, digit_label
         else:
-            return image, (digit_label, color_label, color_dict)
+            return image, (digit_label, color_label, color_label_random)
+
+
+# https://www.webucator.com/article/python-color-constants-module/
+BLUE = [0.0, 0.0, 255.0]
+GREEN = [0.0, 128.0, 0.0]
+ORANGE = [255.0, 128.0, 0]
+YELLOW = [255.0, 255.0, 0]
+VIOLET = [238.0, 130.0, 238.0]
+PINK = [255.0, 192.0, 203.0]
+WHITE = [255.0, 255.0, 255.0]
+RED = [255.0, 0.0, 0.0]
+BROWN = [165.0, 42.0, 42.0]
+MOCCASIN = [255.0, 228.0, 181.0]
+
+color_dict = {
+    0: MOCCASIN,
+    1: BROWN,
+    2: RED,
+    3: WHITE,
+    4: PINK,
+    5: VIOLET,
+    6: YELLOW,
+    7: ORANGE,
+    8: GREEN,
+    9: BLUE,
+}
+color_dict_str = {
+    0: "MOCCASIN",
+    1: "BROWN",
+    2: "RED",
+    3: "WHITE",
+    4: "PINK",
+    5: "VIOLET",
+    6: "YELLOW",
+    7: "ORANGE",
+    8: "GREEN",
+    9: "BLUE",
+}
