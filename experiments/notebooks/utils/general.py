24f04ee3985a63a5a3f697e5f26f82fa24d88ea6
metodj <jazbec.metod@gmail.com>
2022-12-16 17:25:19 +0100

    regression cleanup

--- experiments/notebooks/utils/general.py
+++ experiments/notebooks/utils/general.py
@@ -14,7 +14,13 @@ from torch.utils.data import Dataset
 from src.data import get_image_dataset, NumpyLoader
 from src.utils.training import setup_training
 from src.utils.notebook_metrics import *
+from src.utils.notebook_metrics_regression import *
 from ml_collections import config_dict
+
+from src.models.pog import calculate_pog_loc_scale
+from src.models.reg_ens import calculate_ens_loc_scale
+from src.models.pon import normal_prod
+from src.models.pon_gmm import monte_carlo_pred
 
 
 COLOR_DICT = {1.0: "green", 0.5: "orange", 0.0: "red"}
@@ -203,7 +209,6 @@ def metrics_df(config, models, X_test, y_test, ens_model, prod_model):
     return agg_df
 
 
-
 def metrics_df_by_name(models, X_test, y_test, ens_model, prod_model):
 
     results_df = pd.DataFrame(columns=['model_name', 'n_members', 
@@ -253,3 +258,131 @@ def metrics_df_by_name(models, X_test, y_test, ens_model, prod_model):
 
     
     return agg_df
+
+
+def get_metrics_regression(models, X_test, y_test, pog_model, pon_model, pogmm_model, ens_model, ensemble_size=5):
+    results_df = pd.DataFrame(columns=['model_name', 'n_members', 'random_seed', 'H', 'mse', 'nll'])
+    
+    s = set(range(ensemble_size))
+    power_set = sum(map(lambda r: list(combinations(s, r)), range(1, len(s)+1)), [])
+
+    for model_name in models:
+        for i in range(1):
+            print(model_name)
+            
+            if "pog" in model_name and not 'pogmm' in model_name:
+                model_type = pog_model
+                state = restore_checkpoint(f'dynNN_results/{model_name}', 1)
+            elif "pon" in model_name:
+                model_type = pon_model
+                state = restore_checkpoint(f'dynNN_results/{model_name}', 1)
+            elif "pogmm" in model_name:
+                model_type = pogmm_model
+                state = restore_checkpoint(f'dynNN_results/{model_name}', 1)
+            elif "ens" in model_name:
+                model_type = ens_model
+                state = restore_checkpoint(f'dynNN_results/{model_name}', 1)["0"]
+                
+            print(state["params"]["logscale"])
+
+            pred_fun = partial(
+                model_type.apply,
+                {"params": state['params'], **state['model_state']},
+                train=False, return_ens_preds=True,
+                method=model_type.pred
+            )
+            _, (locs, scales) = jax.vmap(
+                pred_fun, out_axes=(0, 1), in_axes=(0,), axis_name="batch"
+            )(jnp.array(X_test))
+
+
+            for indices in power_set:
+                n_members = len(indices)
+
+                if 'pog' in model_name and not 'pogmm' in model_name:
+                    loc, scale = calculate_pog_loc_scale(locs[indices, :, 0], scales[indices, :, 0])
+                    # loc, scale = calculate_ens_loc_scale(locs[indices, :, 0], scales[indices, :, 0], 1/n_members)
+                    scale_eps = scale # TODO: epistemic uncertainty for PoG
+                    entropies = jax.vmap(uniform_entropy)(loc, scale)
+                    nlls_ = jax.vmap(uniform_nll)(loc, scale, jnp.array(y_test))
+                    infs = jnp.isinf(nlls_)
+                    print(f"dropping {infs.sum()} infs for prod of {n_members}")
+                    nlls = nlls_[~infs]
+                elif 'pon' in model_name:
+                    # loc, scale = normal_prod(locs[indices, :, 0], scales[indices, :, 0], 1/n_members)
+                    loc, scale, scale_eps = calculate_ens_loc_scale(locs[indices, :, 0], scales[indices, :, 0], 
+                                                        1/n_members,
+                                                        epistemic=True)
+                    entropies = jax.vmap(normal_entropy)(loc, scale)
+                    nlls_ = jax.vmap(normal_nll)(loc, scale, jnp.array(y_test))
+                    infs = jnp.isinf(nlls_)
+                    assert infs.sum() == 0
+                    # print(f"dropping {infs.sum()} infs for pon of {n_members}")
+                    nlls = nlls_[~infs]
+                elif 'ens' in model_name:
+                    loc, scale, scale_eps = calculate_ens_loc_scale(locs[indices, :, 0], scales[indices, :, 0], 
+                                                                    1/n_members,  epistemic=True)
+                    entropies = jax.vmap(normal_entropy)(loc, scale)
+                    nlls = jax.vmap(normal_nll)(loc, scale, jnp.array(y_test))
+                elif 'pogmm' in model_name:
+                    _locs, _scales = jnp.transpose(locs[indices, :, :], axes=[1, 0, 2]), jnp.transpose(scales[indices, :, :], axes=[1, 0, 2])
+                    loc, scale = jax.vmap(monte_carlo_pred, in_axes=(0, 0, None))(_locs, _scales, 1/n_members)
+                    entropies = jax.vmap(normal_entropy)(loc, scale)
+                    nlls_ = jax.vmap(normal_nll)(loc, scale, jnp.array(y_test))
+                    infs = jnp.isinf(nlls_)
+                    print(f"dropping {infs.sum()} infs for pon of {n_members}")
+                    nlls = nlls_[~infs]
+                    scale_eps = scale # TODO: epistemic uncertainty for PoG
+                else:
+                    raise ValueError()
+
+                errors = jax.vmap(mse)(loc, jnp.array(y_test))
+
+                results_df = pd.concat([
+                    results_df,
+                    pd.DataFrame({
+                        'model_name': [model_name],
+                        'n_members': [n_members],
+                        'random_seed': [i],
+                        'H': [entropies.mean()],
+                        'nll': [nlls.mean()],
+                        'mse': [errors.mean()],
+                        'epistemic_uncertainty': [scale_eps.mean()],
+                        'aleatoric_uncertainty': [(scale - scale_eps).mean()]
+                    })],
+                    ignore_index=True
+                )
+                
+    min_mse_df = results_df[results_df.n_members == 5][['model_name', 'random_seed', 'mse', 'nll']].rename(
+    columns={'mse': 'final_mse', 'nll': 'final_nll'}
+)
+    tmp_df = results_df.merge(min_mse_df, on=['model_name', 'random_seed'], how='left')
+    tmp_df['mse_diff'] = tmp_df['mse'] - tmp_df['final_mse'] 
+    tmp_df['nll_diff'] = tmp_df['nll'] - tmp_df['final_nll'] 
+    
+    agg_df = tmp_df.groupby(by=['model_name', 'n_members']).agg({
+    'H': ['mean', 'std', 'count'],
+    'mse_diff': ['mean', 'std', 'count'],
+    'mse': ['mean', 'std', 'count'],
+    'nll_diff': ['mean', 'std', 'count'],
+    'nll': ['mean', 'std', 'count'],
+    'epistemic_uncertainty': ['mean', 'std', 'count'],
+    'aleatoric_uncertainty': ['mean', 'std', 'count'],  
+})
+    agg_df[('H', 'std_err')] = agg_df[('H', 'std')] / agg_df[('H', 'count')]
+    agg_df[('mse_diff', 'std_err')] = agg_df[('mse_diff', 'std')] / agg_df[('mse_diff', 'count')]
+    agg_df[('mse', 'std_err')] = agg_df[('mse', 'std')] / agg_df[('mse', 'count')]
+    agg_df[('nll_diff', 'std_err')] = agg_df[('nll_diff', 'std')] / agg_df[('nll_diff', 'count')]
+    agg_df[('nll', 'std_err')] = agg_df[('nll', 'std')] / agg_df[('nll', 'count')]
+    agg_df[('epistemic_uncertainty', 'std_err')] = agg_df[('epistemic_uncertainty', 'std')] / agg_df[('epistemic_uncertainty', 'count')]
+    agg_df[('aleatoric_uncertainty', 'std_err')] = agg_df[('aleatoric_uncertainty', 'std')] / agg_df[('aleatoric_uncertainty', 'count')]
+    
+    return agg_df
+
+
+def generate_name_regression(model_type, i, members_ll, alpha, beta, ft=None):
+    members_ll = members_ll if members_ll else ""
+    alpha = alpha if alpha is not None else ""
+    beta = beta if beta is not None else ""
+    pretrained = f"__pretrained_{ft}" if ft is not None else ""
+    return f"{model_type}_model_{i}_{members_ll}_{alpha}_{beta}".rstrip("_") + pretrained
